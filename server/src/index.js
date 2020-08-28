3acd303514ed585a059986062dc3f1cdd8750437
Yevhwnii <mirandoos3223@gmail.com>
2020-08-28 17:22:44 +0300

    basic setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const middlewares = require('./middlewares');
+const logRouter = require('./api/logs');
 
 const app = express();
 const port = process.env.PORT || 1337;
@@ -12,7 +16,7 @@ app.use(morgan('common'));
 app.use(helmet());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
   })
 );
 
@@ -22,9 +26,15 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/logs', logRouter);
+
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 app.listen(port, () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log(`Listeting on port http://localhost/${port}`);
 });
