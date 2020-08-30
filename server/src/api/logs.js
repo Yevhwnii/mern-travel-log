5f4061aa845a6ea6f37f559597b66347cc82d7d7
Yevhwnii <mirandoos3223@gmail.com>
2020-08-30 13:51:11 +0300

    logs routes

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'GET /',
-  });
+const LogEntry = require('../models/LogEntry');
+
+router.get('/', async (req, res) => {
+  try {
+    const logs = await LogEntry.find();
+    res.json(logs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const result = await logEntry.save();
+    res.json({ message: 'Created!', log: result });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
 });
 
 module.exports = router;
