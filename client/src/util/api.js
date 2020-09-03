cf69e0f108e95840eb6f384a48763c9cb46c1a2a
Yevhwnii <mirandoos3223@gmail.com>
2020-09-03 17:30:21 +0300

    create log entry

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -4,3 +4,14 @@ export const listLogEntries = async () => {
   const response = await fetch(`${API_URL}/api/logs`);
   return await response.json();
 };
+
+export const createLogEntry = async (entry) => {
+  const response = await fetch(`${API_URL}/api/logs`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(entry),
+  });
+  return await response.json();
+};
